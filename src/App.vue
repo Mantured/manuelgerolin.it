7f7150f48370fc0b78a9c0efcf8a2224e05d0d54
MantuRed <manuel31gerolin@gmail.com>
2022-06-01 15:42:31 +0200

    init my personal website

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Main from "./components/Main.vue";
+</script>
+
+<template>
+  <Main msg="Sito personale del Rosso" />
+</template>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+#app {
+  font-family: "Red Hat Display", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
